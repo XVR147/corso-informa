fca878241b2afd51ab604bfe59ad998019ba3052
mirko <mirko.greco16@gmail.com>
2020-05-22 13:28:59 +0200

    esercizio interfaccia geometria

---
+++ interface.php
@@ -0,0 +1,73 @@
+<?php
+
+interface geom{
+
+	public function area();
+	public function perimetro();
+	public function volume();
+	public function diagonale();
+
+}
+
+class quadril implements geom{
+
+	public $altezza;
+	public $base;
+	public $lato1;
+	public $lato2; //base minore
+	public $lato3;
+	public $altSol;
+
+	public function area(){
+		$area = $this->base * $this->altezza;
+		return $area;
+	}
+
+	public function perimetro(){
+		$perimetro = $this->base + $this->lato1 + $this->lato2 + $this->lato3;
+		return $perimetro;
+
+	}
+
+	public function volume(){
+		$volume = $this->area() * $this->altSol;
+		return $volume;
+	}
+
+	public function diagonale(){
+		$diagonale = sqrt(($this->lato1 * $this->lato1)+($this->base * $this->base));
+		return $diagonale;
+
+	}
+
+
+}
+
+class trapezio extends quadril{
+
+
+
+	public function area(){
+
+		$area = (($this->base + $this->lato2) * $this->altezza) / 2;
+		return $area;
+	}
+
+
+
+}
+
+$rettangolo = new quadril();
+$rettangolo->altezza = 5;
+$rettangolo->base = 3;
+//echo $rettangolo->area();
+//echo $rettangolo->altezza;
+
+
+$trapezio = new trapezio();
+$trapezio->base = 12;
+$trapezio->lato2 = 8;
+$trapezio->altezza = 6;
+echo $trapezio->area();
+
+?>
